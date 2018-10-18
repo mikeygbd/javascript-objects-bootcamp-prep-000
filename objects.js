e91c7c6ec4fe5d43b7ce7090eaed7789c1878562
Michael Soares <mikey.soares@gmail.com>
2018-10-18 20:03:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,11 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
     return playlist
 }
-var playlist = {
-  ["artist", "Slowdrive", "Phil Ochs", "My Bloody Valentine"]
-}
 function removeFromPlaylist(playlist, artistName) {
-  {playlist ("artist", "Slowdrive", "Phil Ochs", "My Bloody Valentine")}
-  delete playlist.artistName;
+ delete playlist[artistName];
   return playlist
 }
