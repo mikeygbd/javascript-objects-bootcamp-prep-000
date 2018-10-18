8f7455abcc33d4ba23b3789c6423d371afa81720
Michael Soares <mikey.soares@gmail.com>
2018-10-18 20:00:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
     return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
+  {playlist ("artist", "Slowdrive", "Phil Ochs", "My Bloody Valentine")}
   delete playlist.artistName;
   return playlist
 }
